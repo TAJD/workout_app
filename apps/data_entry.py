6610609a18327edaf092b878eb3992034ae1463c
Thomas Dickson <thomas.dickson@dynamon.co.uk>
2021-05-16 16:53:02 +0100

    weekly stats update

---
+++ apps/data_entry.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pathlib import Path
+import sqlite3
+from sqlite3 import Connection
+import streamlit as st
+
+
+URI_SQLITE_DB = "test.db"
+
+def init_db(conn: Connection):
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS exercises
+        (
+            name TEXT
+            type TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS record
+        (
+            date TEXT
+            
+        )
+        """
+    )
+    conn.commit()
+
+
+if __name__ == "__main__":
+    pass
